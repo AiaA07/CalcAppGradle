c5015086db451210def384cbc54a58e8db8bcbc5
Blossom-Anolue <blossomanolue4@gmail.com>
2024-12-07 17:50:07 -0500

    More Integration tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import java.awt.event.ActionEvent;
 
 
 @RunWith(JUnit4.class)
@@ -14,6 +13,18 @@ public class IntegrationTest {
     @Before
     public void init(){
         calculator = new Main();
+    }
+
+    /*
+     * Input: 123
+     * Expected Output: 123.0 */
+    @Test
+    public void testNumberInput() {
+        calculator.textField.setText("1");
+        calculator.textField.setText("2");
+        calculator.textField.setText("3");
+        calculator.equButton.doClick();
+        Assert.assertEquals("123.0", calculator.textField.getText());
     }
     /*
      * Input: 2 + 3
@@ -27,6 +38,50 @@ public class IntegrationTest {
         Assert.assertEquals("5.0", calculator.textField.getText());
     }
     /*
+     * Input: 5-3
+     * Expected Output: 2.0*/
+    @Test
+    public void testSubtractionIntegration() {
+        calculator.textField.setText("5");
+        calculator.subButton.doClick();
+        calculator.textField.setText("3");
+        calculator.equButton.doClick();
+        Assert.assertEquals("2.0", calculator.textField.getText());
+    }
+    /*
+     * Input: 2*4
+     * Expected Output: 8.0*/
+    @Test
+    public void testMultiplicationIntegration() {
+        calculator.textField.setText("4");
+        calculator.mulButton.doClick();
+        calculator.textField.setText("2");
+        calculator.equButton.doClick();
+        Assert.assertEquals("8.0", calculator.textField.getText());
+    }
+    /*
+     * Input: 9/3
+     * Expected Output: 3.0*/
+    @Test
+    public void testDivisionIntegration() {
+        calculator.textField.setText("9");
+        calculator.divButton.doClick();
+        calculator.textField.setText("3");
+        calculator.equButton.doClick();
+        Assert.assertEquals("3.0", calculator.textField.getText());
+    }
+    /*
+     * Input: 3/0
+     * Expected Output: Error */
+    @Test
+    public void testDivisionByZero() {
+        calculator.textField.setText("9");
+        calculator.divButton.doClick();
+        calculator.textField.setText("0");
+        calculator.equButton.doClick();
+        Assert.assertEquals("Error: Division by zero", calculator.textField.getText());
+    }
+    /*
      * Input: 3!
      * Expected Output: 6.0 */
     @Test
@@ -35,6 +90,17 @@ public class IntegrationTest {
         calculator.factButton.doClick();
         calculator.equButton.doClick();
         Assert.assertEquals("6.0", calculator.textField.getText());
+    }
+    /*
+     * Input: -3!
+     * Expected Output: Error */
+    @Test
+    public void testNegativeFactorialIntegration() {
+        calculator.negButton.doClick();
+        calculator.textField.setText("3");
+        calculator.factButton.doClick();
+        calculator.equButton.doClick();
+        Assert.assertEquals("Error: Factorial of a negative number", calculator.textField.getText());
     }
     /*
      * Input: 3^2
@@ -51,7 +117,7 @@ public class IntegrationTest {
     /*
      * Input: 5%
      * Expected Output: 0.05 */
-    @org.junit.jupiter.api.Test
+    @Test
     public void testPercentageIntegration() {
         calculator.textField.setText("5");
         calculator.percButton.doClick();
@@ -68,6 +134,87 @@ public class IntegrationTest {
         calculator.equButton.doClick();
         Assert.assertEquals("4.0",calculator.textField.getText());
     }
+    /*
+     * Input: sqrt(-16)
+     * Expected Output: Invalid square root */
+    @Test
+    public void testNegativeSquareRoot() {
+        calculator.negButton.doClick();
+        calculator.textField.setText("16");
+        calculator.sqrtButton.doClick();
+        calculator.equButton.doClick();
+        Assert.assertEquals("Invalid square root of a negative", calculator.textField.getText());
+    }
+
+    //TESTING COMBINATIONS OF DIFFERENT OPERATIONS.
+
+    /*
+     * Input: 3+20%
+     * Expected Output: 3,60 */
+    @Test
+    public void testAdditionThenPercentage() {
+        calculator.textField.setText("3");
+        calculator.addButton.doClick();
+        calculator.textField.setText("20");
+        calculator.percButton.doClick();
+        calculator.equButton.doClick();
+        Assert.assertEquals("3.60",calculator.textField.getText());
+    }
+    /*
+     * Input: 2^3-4
+     * Expected Output: 4.0 */
+    @Test
+    public void testExponentThenSubtraction() {
+        calculator.textField.setText("2");
+        calculator.expoButton.doClick();
+        calculator.textField.setText("3");
+        calculator.subButton.doClick();
+        calculator.textField.setText("4");
+        calculator.equButton.doClick();
+        Assert.assertEquals("4.0",calculator.textField.getText());
+    }
+    /*
+     * Input: sqrt(16) + 5
+     * Expected Output: 9.0 */
+    @Test
+    public void testSquareRootThenAddition() {
+        calculator.textField.setText("16");
+        calculator.sqrtButton.doClick();
+        calculator.addButton.doClick();
+        calculator.textField.setText("5");
+        calculator.equButton.doClick();
+        Assert.assertEquals("9.0",calculator.textField.getText());
+    }
+    /*
+     * Input: 4!-2
+     * Expected Output: 22.0 */
+    @Test
+    public void testFactorialThenSubtraction() {
+        calculator.textField.setText("4");
+        calculator.factButton.doClick();
+        calculator.subButton.doClick();
+        calculator.textField.setText("2");
+        calculator.equButton.doClick();
+        Assert.assertEquals("22.0",calculator.textField.getText());
+    }
+
+    @Test
+    public void testClear() {
+        calculator.textField.setText("1");
+        calculator.textField.setText("2");
+        calculator.textField.setText("3");
+        calculator.clrButton.doClick();
+        Assert.assertEquals(" ",calculator.textField.getText());
+    }
+    @Test
+    public void testDelete() {
+        calculator.textField.setText("1");
+        calculator.textField.setText("2");
+        calculator.textField.setText("3");
+        calculator.delButton.doClick();
+        Assert.assertEquals("12",calculator.textField.getText());
+    }
 
 }
+
 
